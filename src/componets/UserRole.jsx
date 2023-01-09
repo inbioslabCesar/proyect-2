065dad4a09a52f9a1058080751921630083ad981
Cesar Perez <inbioslab@gmail.com>
2023-01-08 20:25:58 -0500

    UserRow state

--- src/componets/UserRole.jsx
+++ src/componets/UserRole.jsx
@@ -1,10 +1,11 @@
 import style from './UserRole.module.css'
+const ROLE_STYLES = {
+	teacher: ['profesor', style.teacher],
+	student: ['alumno', style.student],
+	other: ['otro', style.other]
+};
 const UserRole = ({role}) => {
-    const ROLE_STYLES = {
-		teacher: ['profesor', style.teacher],
-		student: ['alumno', style.student],
-		other: ['otro', style.other]
-	};
+   
 	const [roleName, roleClassname] = ROLE_STYLES[role] || ROLE_STYLES.other;
     
     return <span className={ `${style.role} ${roleClassname}`}>{roleName}</span>
